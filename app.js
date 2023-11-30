2b0228855f3688520104f7a2c6fd8ec14b6a84e8
Jure <jure.peros@gmail.com>
2023-11-30 15:10:01 +0100

    handled arraysw

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ import * as fs from "fs"
 
 const data = JSON.parse(fs.readFileSync("./primeri/helloWorld.json"))
 
-let allThings = []
-let howDeep = 0
 let metaStringy = ""
+
+//globinomer
+let howDeep = 0
+
+// allThings je glavni arr; theKey shranjuje enake vrednosti katere nato
+// uporabim za closing tage
+let allThings = []
 let theKey = []
 
 function traverse(obj) {
@@ -31,30 +36,57 @@ function traverse(obj) {
     for (const key in obj) {
         const value = obj[key]
 
-        //console.log(howDeep, [key])
+        if (Array.isArray(value)) {
+            value.forEach((element) => {
+                metaStringy += `\n<${key}`
+                for (const elementKey in element) {
+                    metaStringy += ` ${elementKey}="${element[elementKey]}"`
+
+                }
+                metaStringy += ">\n"
+            })
+        return
+        }
+
+       // console.log(howDeep, [key])
+
         allThings.push([howDeep, key])
         theKey.push(["keyed", key])
+
 
         if (typeof value === "object") {
             howDeep++
             traverse(value)
-        } else if (Array.isArray(value)) {
-            for (const element of value) {
-                howDeep++
-                traverse(element)
-            }
         } else {
+            const popedKey = allThings.pop()
 
-            allThings.push([howDeep, value])
+            if (popedKey[1] != "href" && popedKey[1] != "rel" && popedKey[1] != "type") {
+                allThings.push([howDeep, `<${popedKey[1]}>${value}</${popedKey[1]}>`])
+
+            } else allThings.push([howDeep, `${popedKey[1]}="${value}"`])
+
         }
         const nestedItem = theKey.pop()
-        allThings.push(nestedItem)
+
+        //console.log(nestedItem[1])
+        //allThings.push(nestedItem)
+        if (nestedItem[1] != "href" && nestedItem[1] != "rel" && nestedItem[1] != "type") {
+            allThings.push(nestedItem)
+        }
+
 
     }
     howDeep--
 }
 
 traverse(data)
-
+console.log(metaStringy)
 console.log(allThings, "\n------------------")
-console.log(theKey)
+//console.log(theKey)
+
+allThings.forEach((element) => {
+    //console.log(element[1])
+    //fs.writeFileSync()
+})
+
+
